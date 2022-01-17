ccee695dd1db4263145849e926afe750207920dc
Cristiano Motta <cristiano.motta@hotmail.it>
2022-01-17 12:18:48 +0100

    Added before unload example

---
+++ react-studio/src/components/beforeUnload.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from "react"
+
+export const BeforeUnload = () => {
+  // Handler che lego all'evento beforeunload
+  const onBeforeOnloadHandler = (evt) => {
+    console.log("Handler called")
+    evt.preventDefault()
+    evt.returnValue = ""
+  }
+
+  // Calling Cleanup before browser or tab closing
+  const subscribe = () => {
+    // Attenzione: devo legare lo stesso handler che poi andrò a rimuovere nella funzione di cleanup
+    // (in questo caso onBeforeOnloadHandler)
+    window.addEventListener("beforeunload", onBeforeOnloadHandler)
+
+    return () => {
+      window.removeEventListener("beforeunload", onBeforeOnloadHandler)
+    }
+  }
+
+  useEffect(subscribe, [])
+
+  return (
+    <div>
+      <p style={{ backgroundColor: "yellow", width: "30%" }}>Prova a chiudere il tab o il browser </p>
+    </div>
+  )
+}
